d036358213b231c758f3b038663107af17800c4c
Logan Rakai <lmrakai@gmail.com>
2018-01-15 09:02:55 -0700

    chapter 5

---
+++ chpt5-exprAggregation/query.js
@@ -0,0 +1,50 @@
+db.air_alliances.aggregate([
+    {
+        "$lookup": {
+            "from": "air_routes",
+            "let": {
+                "airlines": "$airlines"
+            },
+            "pipeline": [{
+                "$match": {
+                    $expr:
+                        {
+                            $and:
+                                [
+                                    { $in: ["$airline.name", "$$airlines"] }
+                                ]
+                        }
+                }
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                    "airplanes": "$airplane"
+                }
+            }
+            ],
+            "as": "fleet"
+        }
+    },
+    {
+        "$project": {
+            "name": 1,
+            "fleet": {
+                "$size": {
+                    "$reduce": {
+                        "input": "$fleet.airplanes",
+                        "initialValue": [],
+                        "in": {
+                            "$setUnion": [
+                                "$$value",
+                                {
+                                    "$split": ["$$this", " "]
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        }
+    }
+])
